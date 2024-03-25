c98225d8be8bf7642e315b2ea9967854cf982e6c
HosseinKolivand128 <ac.hossein.ac@gmail.com>
2024-03-25 21:42:31 +0330

    made some changes

--- next/app/accounts/[profile]/names/[name]/page.tsx
+++ next/app/accounts/[profile]/names/[name]/page.tsx
@@ -1,7 +1,5 @@
 
 export default function names({params}:{params:{name: string, profile:string}}) {
-// console.log(params);
-
     return (
         <h1>
             Profile {params.profile} about account with name: {params.name}
