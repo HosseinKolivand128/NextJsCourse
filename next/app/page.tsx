3c6324bde3937d6e19ca249a56a58102a69f7088
HosseinKolivand128 <ac.hossein.ac@gmail.com>
2024-03-29 14:22:09 +0330

    added links

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,8 +1,17 @@
+import Link from "next/link"
 import "./page.module.css"
 export default function Home(){
   return (
+    <>
     <h1>
-      Hello world
+      Welcome home!
     </h1>
+    <Link href="/about">
+      about
+    </Link>
+    <Link href="/products">
+      products
+    </Link>
+    </>
   )
 }
--- next/app/products/page.tsx
+++ next/app/products/page.tsx
@@ -1,12 +1,24 @@
+import Link from "next/link";
+
 export default function ProductList(){
+    const productId=15;
     return (
         <>
         <h1>
             Product List
         </h1>
-        <h2>Product 1</h2>
-        <h2>Product 2</h2>
-        <h2>Product 3</h2>
+        <h2><Link href="/products/1">Product 1</Link></h2>
+        <h2><Link href="/products/2">Product 2</Link></h2>
+        <h2><Link href="/products/3" replace>Product 3</Link></h2>
+        {/* replace will remove the last page from the history and replace the current page instead of that(search more) */}
+
+        <h2>
+            <Link href={`/products/${productId}`} >
+            {`Product ${productId}`}
+            </Link>
+        </h2>
+
+
         </>
     )
 }
