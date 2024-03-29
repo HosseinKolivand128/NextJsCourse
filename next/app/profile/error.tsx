e134c2f200586682758d0cbc8fd0e89848d725fc
HosseinKolivand128 <ac.hossein.ac@gmail.com>
2024-03-29 22:33:26 +0330

    update to recovering from errors

--- next/app/profile/error.tsx
+++ next/app/profile/error.tsx
@@ -1,5 +1,10 @@
 "use client";
 import "./styles.css"
-export default function ErrorBoundary({error}:{error:Error}){
-    return <h1 className="Error">{error.message}</h1>
+export default function ErrorBoundary({error,reset}:{error:Error,reset:()=>void}){
+    return(
+        <>
+<h1 className="Error">{error.message}</h1>
+<button onClick={reset}>try again</button>
+        </>
+    )
 }
